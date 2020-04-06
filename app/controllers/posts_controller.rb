d38d0be78fe643bc559307597281808ca4050ae2
Alice Freda <alicefreda@gmail.com>
2020-04-05 18:59:13 -0700

    new post and post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
     def index
-        @Posts = Post.all
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            redirect_to post_path(@post)
+        else
+            flash[:message] = "Your post could not be created."
+            render :new
+        end
+    end
+
+    def show
+        @post = Post.find_by_id(params[:id])
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content, :category_id)
     end
 end
